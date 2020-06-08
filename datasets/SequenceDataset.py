9bf8904805af6a29d7c9f88768fce159659f4bd3
EricElmoznino <eric.elmoznino@gmail.com>
2020-06-08 18:19:22 -0400

    Modified vvgqn dataset so that rotation is returned directly, as opposed to sin/cos transformation of it. This way, the amount of rotation/change is proportional to the magnitutde of the rotation

--- datasets/SequenceDataset.py
+++ datasets/SequenceDataset.py
@@ -10,7 +10,7 @@ from datasets.path_generators import noisy_path
 class SequenceDataset(Dataset):
 
     def __init__(self, path_length=20, width=10, height=10, n_samples=1000):
-        self.v_dim = 3
+        self.v_dim = 2
         self.n_samples = n_samples
         self.env = SimpleRectangularEnvironment(width, height)
         self.path_length = path_length
@@ -27,7 +27,7 @@ class SequenceDataset(Dataset):
 
         movements = torch.from_numpy(path['movements'])
         rotations = torch.from_numpy(path['rotations'])
-        vestibular = torch.stack([movements, rotations.cos(), rotations.sin()], dim=1)
+        vestibular = torch.stack([movements, rotations], dim=1)
 
         if return_path:
             positions = torch.from_numpy(path['positions'])
